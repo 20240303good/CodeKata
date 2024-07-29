1c4d2a7e4147fda08e6e9980d25228a7661aced9
beom <162035439+20240303good@users.noreply.github.com>
2024-07-29 11:12:54 +0900

    [level 0] Title: a와 b 출력하기, Time: 250.54 ms, Memory: 47.9 MB -BaekjoonHub

--- 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.kt
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
     val (a, b) = readLine()!!.split(' ').map(String::toInt)
-    println("a = $a")
-    println("b = $b")
+    println("a= $a")
+    println("b= $b")
 }
 
 
